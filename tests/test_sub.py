73da5b900799190f6e676a9721239877a567d118
jeretamargo <jeretamargo01@gmail.com>
2026-04-17 09:40:38 -0300

    documentacion agregada  test_sub

--- tests/test_sub.py
+++ tests/test_sub.py
@@ -20,6 +20,14 @@ def test_sub_resta_positivos():
 #
 # Pista: podés usar @pytest.mark.parametrize para probar varios casos a la vez.
 
+"""
+Este test parametrizado realiza 4 vueltas
+1era vuelta: comprueba que 5 - 4 devuelva 1
+2da vuelta: comprueba que 0 - 4 devuelva -4
+3era vuelta: comprueba que -4 - (-4) devuelva 0
+4ta vuelta: comprueba que -1.5 - (-1.5) devuelva 0
+"""
+
 @pytest.mark.parametrize("a,b,expected", [(5, 4, 1),(0, 4, -4),(-4,-4,0),(-1.5,-1.5,0)])
 def test_add_parametrizado(a, b, expected):
      assert sub(a, b) == expected

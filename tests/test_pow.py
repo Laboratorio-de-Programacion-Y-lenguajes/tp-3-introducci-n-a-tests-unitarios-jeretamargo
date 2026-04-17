fb12b3888262a1c53ad049c4fc481076198e40f5
jeretamargo <jeretamargo01@gmail.com>
2026-04-17 10:16:00 -0300

    test_sqrt_y_pow_realizado

--- tests/test_pow.py
+++ tests/test_pow.py
@@ -19,3 +19,15 @@ def test_pow_base_positiva():
 #   - Exponente decimal, ej: 9 ** 0.5 (raíz cuadrada)
 #
 # Pista: podés usar @pytest.mark.parametrize para probar varios casos a la vez.
+
+"""
+Este test parametrizado realiza 4 vueltas:
+1era vuelta: comprueba que 5^0 devuelva 1 (numero elevado a la 0 = 1)
+2da vuelta: comprueba que 5^1 devuelva 5 (numero elevado a la 1 = mismo número)
+3era vuelta: comprueba que -4^2 devuelva  16 (resultado positivo)
+4ta vuelta: comprueba que  devuelva 9^1.5 devuelva 3 (raíz cuadrada)
+"""
+
+@pytest.mark.parametrize("a,b,expected", [(5, 0, 1),(5, 1, 5),(-4, 2, 16),(9 , 0.5 , 3)])
+def test_add_parametrizado(a, b, expected):
+     assert pow_(a, b) == expected
--- tests/test_sqrt.py
+++ tests/test_sqrt.py
@@ -1,7 +1,7 @@
 """Tests para la función sqrt(x) -> float."""
 
 import pytest
-
+import math
 from src.calculator import sqrt
 
 
@@ -22,3 +22,22 @@ def test_sqrt_cuadrado_perfecto():
 # def test_sqrt_negativo():
 #     with pytest.raises(ValueError):
 #         sqrt(-4)
+
+"""
+Esta serie de testeos tiene una funcion parametrizada con 2 vueltas:
+1era vuelta: comprueba la raiz de cero devuelva cero
+2da vuelta: comprueba que la raiz de un numero impar devuelva decimal
+"""
+
+@pytest.mark.parametrize("a,expected", [(0,0.0 ),(5, math.sqrt(5))])
+def test_add_parametrizado(a, expected):
+     assert sqrt(a) == expected
+
+def test_sqrt_negativo():
+     """
+     Este test comprueba que al intentar realizar la raiz de
+     un numero negativo, la funcion lance una excepción de tipo
+     "ValueError"
+     """
+     with pytest.raises(ValueError):
+         sqrt(-4)

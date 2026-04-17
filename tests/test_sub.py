f2b257457f5e73d1616a0e89c98622c079ff615c
jeretamargo <jeretamargo01@gmail.com>
2026-04-17 09:48:20 -0300

    test_mul realizado con parametrize y documentacion modificada

--- tests/test_sub.py
+++ tests/test_sub.py
@@ -21,14 +21,14 @@ def test_sub_resta_positivos():
 # Pista: podés usar @pytest.mark.parametrize para probar varios casos a la vez.
 
 """
-Este test parametrizado realiza 4 vueltas
-1era vuelta: comprueba que 5 - 4 devuelva 1
-2da vuelta: comprueba que 0 - 4 devuelva -4
-3era vuelta: comprueba que -4 - (-4) devuelva 0
-4ta vuelta: comprueba que -1.5 - (-1.5) devuelva 0
+Este test parametrizado realiza 4 vueltas:
+1era vuelta: comprueba que 5 - 6 devuelva -1 (resultado negativo)
+2da vuelta: comprueba que 0 - 4 devuelva -4 (Restar cero)
+3era vuelta: comprueba que -4 - (-4) devuelva 0 (Restar dos números negativos)
+4ta vuelta: comprueba que -1.5 - (-1.5) devuelva 0 (Restar dos números decimales)
 """
 
-@pytest.mark.parametrize("a,b,expected", [(5, 4, 1),(0, 4, -4),(-4,-4,0),(-1.5,-1.5,0)])
+@pytest.mark.parametrize("a,b,expected", [(5, 6, -1),(0, 4, -4),(-4,-4,0),(-1.5,-1.5,0)])
 def test_add_parametrizado(a, b, expected):
      assert sub(a, b) == expected
 
